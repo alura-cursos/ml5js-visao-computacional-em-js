719fdb45f25ffdf521ba70da3c6a5dc2023413f8
Guilherme Silveira <guilherme.silveira@gmail.com>
2023-01-18 15:26:01 -0300

    4.2

--- palavras.js
+++ palavras.js
@@ -4,10 +4,19 @@ export class Palavra {
         this.palavra = palavra;
         this.posicao = posicao;
         this.tamanho = height / 10;
+        this.tamanhoInicial = this.tamanho;
         console.log(`criando ${palavra}`);
     }
 
-    desenha = function() {
+    atualiza = function() {
+        const segundosPassados = deltaTime / 1000;
+
+        const velocidadeDeEncolhimento = this.tamanhoInicial / 6;
+        this.tamanho -= velocidadeDeEncolhimento * segundosPassados;
+
+        this.posicao.x += 50 * segundosPassados;
+        this.posicao.y += 50 * segundosPassados;
+
         fill(color("#6cd9ff"));
 
         textSize(this.tamanho);
@@ -17,7 +26,6 @@ export class Palavra {
         textAlign(CENTER, CENTER);
 
         text(this.palavra, this.posicao.x, this.posicao.y);
-        this.tamanho -= 1;
     }
 
     estaViva = function() {
--- sketch.js
+++ sketch.js
@@ -39,7 +39,7 @@ window.draw = function() {
 
 
     for(const palavra of palavras) {
-        palavra.desenha();
+        palavra.atualiza();
     }
     palavras = palavras.filter(p => p.estaViva())
 
